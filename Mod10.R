cf3907d673ef332e01674c93d56a7db318e43036
Nollidw <107093946+Nollidw@users.noreply.github.com>
2023-10-29 16:53:14 -0400

    Add files via upload

---
+++ Mod10.R
@@ -0,0 +1,8 @@
+Package: WilliamsPackage
+Title: 
+Version: 0.1
+Authors@R: "Dillon Williams","dillonwilliams@usf.edu" role = c("aut", "cre")
+Description: This package is designed to 
+Depends: R (>= 3.1.2)
+License: CC0 1.0 DEED
+LazyData: true
